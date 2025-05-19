bffd341bf58970017f52884b005d2679c2c87cd9
Aman Kumar Jagdev <amankumarjagdev@gmail.com>
2025-05-19 12:55:48 +0530

    Update getHillUniversities function to return a static list of hill universities, removing dependency on getAvailableUniversities.

--- lib/excel.ts
+++ lib/excel.ts
@@ -268,9 +268,12 @@ export function getAvailableUniversities(): string[] {
 }
 
 export function getHillUniversities(): string[] {
-    const universities = getAvailableUniversities();
+    return ["ddn", "btl", "hld"];
+
+    // // const universities = getAvailableUniversities();
+
     //Because all universities are hill universities now
-    return universities;
+    // // return universities;
     // return universities.filter((university) =>
     //     university.toLowerCase().includes("gehu")
     // );
