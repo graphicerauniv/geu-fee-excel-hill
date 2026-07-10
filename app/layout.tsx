1fa389f92115e36182cc7efcbfb8a3dee229ff3c
Anubhav-dhyani <adhyani1105@gmail.com>
2026-07-10 11:20:41 +0530

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
@@ -27,9 +28,26 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <Script id="google-tag-manager" strategy="beforeInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MTTTBJ88');`}
+                </Script>
+            </head>
             <body
                 className={`${poppins.variable} ${geistMono.variable} antialiased`}
             >
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-MTTTBJ88"
+                        height="0"
+                        width="0"
+                        style={{ display: "none", visibility: "hidden" }}
+                    />
+                </noscript>
                 {children}
 
                 <Toaster />
